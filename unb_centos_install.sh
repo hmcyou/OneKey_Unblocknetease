6ad5d6bb92584387b60b324824b5d39008c74ebf
NewCheung <41122937+NewCheung@users.noreply.github.com>
2020-05-27 10:56:58 +0800

    Update unb_centos_install.sh

--- unb_centos_install.sh
+++ unb_centos_install.sh
@@ -1,11 +1,7 @@
-curl -sL https://rpm.nodesource.com/setup_10.x | bash -  &&
-yum -y install nodejs &&
 yum -y install git
-
+curl -sL https://rpm.nodesource.com/setup_10.x | bash -
+yum -y install nodejs
 git clone https://github.com/nondanee/UnblockNeteaseMusic.git
-
 mv UnblockNeteaseMusic /usr
-
 wget https://raw.githubusercontent.com/NewCheung/OneKey_Unblocknetease/master/unb.service
-
 mv unb.service /lib/systemd/system
